0f7670d098122633342e5c64a0a28b58249dcc31
irene.bitlloch <irene.bitlloch@gmail.com>
2017-12-22 16:20:34 +0100

    compila!!

--- app/src/main/java/com/example/irenebitlloch/provavideo/MainActivity.java
+++ app/src/main/java/com/example/irenebitlloch/provavideo/MainActivity.java
@@ -1,58 +1,52 @@
 package com.example.irenebitlloch.provavideo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Rational;
+import org.jcodec.common.model.Rational;
 
 import org.jcodec.api.FrameGrab;
 import org.jcodec.api.SequenceEncoder;
+import org.jcodec.api.android.AndroidSequenceEncoder;
+import org.jcodec.common.AndroidUtil;
 import org.jcodec.common.RunLength;
 import org.jcodec.common.io.NIOUtils;
 
 import java.io.File;
-import java.nio.channels.SeekableByteChannel;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.jcodec.common.io.SeekableByteChannel;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         SeekableByteChannel out = null;
         try {
             out = NIOUtils.writableFileChannel("/tmp/output.mp4");
-            // for Android use: AndroidSequenceEncoder
-            AWTSequenceEncoder encoder = new AWTSequenceEncoder(out, Rational.R(25, 1));
+
+            AndroidSequenceEncoder encoder = new AndroidSequenceEncoder(out, Rational.R(25, 1));
             for (int i =0; i < Fotos.length; i++) {
-
-
                 // Generate the image, for Android use Bitmap
-                BufferedImage image = Fotos[i];
+                Bitmap image = BitmapFactory.decodeResource(getResources(), Fotos[i]);
                 // Encode the image
                 encoder.encodeImage(image);
             }
             // Finalize the encoding, i.e. clear the buffers, write the header, etc.
             encoder.finish();
-        }
-        finally {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             NIOUtils.closeQuietly(out);
         }
-
-
-
-
 
     }
 
